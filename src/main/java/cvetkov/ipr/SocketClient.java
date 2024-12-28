7753a1b2fcd2f68b5dcb81fae29abbfcf2d43062
YuryTs <tsvetkov_y_g@mail.ru>
2024-12-28 10:56:56 +0300

    ServerSocket and SocketClient 1

--- src/main/java/cvetkov/ipr/SocketClient.java
+++ src/main/java/cvetkov/ipr/SocketClient.java
@@ -1,9 +1,9 @@
 package cvetkov.ipr;
 
-import java.io.*;
+import java.io.BufferedReader;
+import java.io.InputStreamReader;
+import java.io.PrintWriter;
 import java.net.Socket;
-import java.net.UnknownHostException;
-import java.util.Scanner;
 import java.util.concurrent.ExecutorService;
 import java.util.concurrent.Executors;
 import java.util.concurrent.TimeUnit;
